bf725b2b4d852bdbfaa448806dcb4d6c2bd1fcdb
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-04-06 09:34:42 +0000

    add creating and recreating silver table, num of loaded rows to metadata table

--- silver/transform.py
+++ silver/transform.py
@@ -11,7 +11,26 @@ TARGET_DB = os.getenv("TARGET_DB")
 TARGET_SCHEMA = os.getenv("TARGET_SCHEMA")
 TARGET_TABLE = os.getenv("TARGET_TABLE")
 
+RESET_TABLES = os.getenv("RESET_TABLES")
+
 METADATA_TABLE = "loads"
+
+TARGET_TABLE_SCHEMA = """
+date DATE,
+time TIME,
+line VARCHAR,
+operator VARCHAR,
+vehicle VARCHAR,
+journey_pattern VARCHAR,
+origin_short_name VARCHAR,
+destination_short_name VARCHAR,
+direction VARCHAR,
+longitude DOUBLE,
+latitude DOUBLE,
+speed REAL,
+origin_aimed_departure_time TIME,
+delay BIGINT,
+"""
 
 if None in [
     SOURCE_DB,
@@ -29,9 +48,24 @@ if None in [
 # Get timestamp of the last load from bronze to silver.
 with duckdb.connect(TARGET_DB) as db:
     db.sql(f"CREATE SCHEMA IF NOT EXISTS {TARGET_SCHEMA}")
-    db.sql(
-        f"CREATE TABLE IF NOT EXISTS {TARGET_SCHEMA}.{METADATA_TABLE} (load_id VARCHAR)"
-    )
+
+    # If we're resetting tables, we recreate the tables and we set the env variable to false afterwards.
+    if RESET_TABLES is not None and RESET_TABLES.lower() == "true":
+        print("Resetting silver tables.")
+        print("*" * 50)
+        db.sql(
+            f"CREATE OR REPLACE TABLE {TARGET_SCHEMA}.{METADATA_TABLE} (load_id VARCHAR, loaded_rows INTEGER)"
+        )
+        db.sql(
+            f"CREATE OR REPLACE TABLE {TARGET_SCHEMA}.{TARGET_TABLE} ({TARGET_TABLE_SCHEMA})"
+        )
+    else:
+        db.sql(
+            f"CREATE TABLE IF NOT EXISTS {TARGET_SCHEMA}.{METADATA_TABLE} (load_id VARCHAR, loaded_rows INTEGER)"
+        )
+        db.sql(
+            f"CREATE TABLE IF NOT EXISTS {TARGET_SCHEMA}.{TARGET_TABLE} ({TARGET_TABLE_SCHEMA})"
+        )
 
     last_load = db.sql(
         f"SELECT MAX(load_id) FROM {TARGET_SCHEMA}.{METADATA_TABLE}"
@@ -56,7 +90,8 @@ if len(source_df) == 0:
     print("No new data to load.")
     sys.exit(0)
 
-print(f"# of new rows: {len(source_df)}")
+new_row_count = len(source_df)
+print(f"# of new rows: {new_row_count}")
 print("*" * 50)
 
 new_df = (
@@ -68,13 +103,11 @@ new_df = (
             "monitored_vehicle_journey__journey_pattern_ref": "journey_pattern",
             "monitored_vehicle_journey__origin_short_name": "origin_short_name",
             "monitored_vehicle_journey__destination_short_name": "destination_short_name",
+            "monitored_vehicle_journey__direction_ref": "direction",
         }
     ).with_columns(
         pl.col("recorded_at_time").cast(pl.Date).alias("date"),
         pl.col("recorded_at_time").cast(pl.Time).alias("time"),
-        pl.col("monitored_vehicle_journey__direction_ref")
-        .cast(pl.Int8)
-        .alias("direction"),
         pl.col("monitored_vehicle_journey__vehicle_location__longitude")
         .cast(pl.Float64)
         .alias("longitude"),
@@ -96,7 +129,8 @@ new_df = (
         .map_batches(
             lambda x: pl.Series(
                 map(delay_sec, x.struct.field("monitored_vehicle_journey__delay"))
-            )
+            ),
+            return_dtype=pl.Int64,
         )
         .alias("delay"),
     )
@@ -125,7 +159,7 @@ print(new_df)
 with duckdb.connect(TARGET_DB) as db:
     db.sql("BEGIN TRANSACTION")
     db.sql(
-        f"""INSERT INTO {TARGET_SCHEMA}.{METADATA_TABLE} (load_id)
-        VALUES ({source_df["_dlt_load_id"].max()})"""
+        f"""INSERT INTO {TARGET_SCHEMA}.{METADATA_TABLE} (load_id, loaded_rows)
+        VALUES ({source_df["_dlt_load_id"].max()}, {new_row_count})"""
     )
     db.sql("COMMIT")
